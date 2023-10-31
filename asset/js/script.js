d41cdfa4859c83053396dbc723173968b74e3fd4
hridoysheikh15 <ridoysheikh15@gmail.com>
2023-10-31 19:59:15 +0600

    oct 31, preloader

---
+++ asset/js/script.js
@@ -0,0 +1,9 @@
+// preloader
+
+window.addEventListener("load", function () {
+    const preloader = document.getElementById("preloader");
+    preloader.classList.add("loaderAnim")
+    this.setTimeout(()=>{
+    preloader.style.display = "none"; // Hide preloader
+    }, 700)
+  });
